6743562c70c097706cd2c743619ee88f3bf427c7
techbaeng <techbaeng@gmail.com>
2021-04-07 12:27:43 +0900

    javascript study

---
+++ JavaScript_Test/WebContent/js/main.js
@@ -0,0 +1,11 @@
+/*
+	누구를 볼려고 클릭했는지는 tabTarget에 저장했으니, 그게 되려면 tab-content안에 있는 div는 다 접어야 함
+	tab-content 안의 div들 모두 안보이도록 한다.
+	
+ 	링크를 클릭하면 클릭한 그 요소의 href속성의 값을 변수 tabTarget에 저장
+ 	저장된 값에서 #을 삭제
+	
+	tabTarget = #tabs-1
+	//document.getElementById(tabs-1).style.display='block';
+	document.getElementById().style.display='block';
+ */
